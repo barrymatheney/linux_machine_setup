a8c1fb71368a1a592dfb6422ea71a8ceaf7cb7f2
Barry Matheney <barry_matheney_msdn@outlook.com>
2020-05-22 21:47:00 -0400

    added helm3.sh and gitignore

---
+++ helm3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
+#https://github.com/helm/helm/releases
+
+url="https://get.helm.sh/"
+targzfilename="helm-v3.2.1-linux-amd64.tar.gz"
+fullurl=$url$targzfilename
+echo $fullurl
+[ -f $targzfilename ] && rm $targzfilename
+wget $fullurl 
+
+outdir="helmunzip"
+[ ! -d $outdir ] && mkdir $outdir
+
+tar -zxvf  ./$targzfilename --directory $outdir
+
+chmod +x $outdir/linux-amd64/helm
+
+sudo mv $outdir/linux-amd64/helm /usr/local/bin/helm
+
+
