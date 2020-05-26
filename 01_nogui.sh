866e269a131a5e1bca249451fc3f3074c5d6157f
= <=>
2020-05-26 08:17:52 -0400

    split the 01.sh out to gui and non-gui

--- 01_nogui.sh
+++ 01_nogui.sh
@@ -3,7 +3,7 @@ printf 'bmatheney ALL=(ALL:ALL) NOPASSWD:ALL\n' | sudo tee -a /etc/sudoers >/dev
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install git gitk htop curl wget -y
+sudo apt install git htop curl wget  -y
 
 git config --global user.name "Barry Matheney"
 git config --global user.email "barry_matheney_msdn@outlook.com"
