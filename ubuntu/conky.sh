6aef5bf25a3e9876c9cc9a3eff095823bb18db8f
Barry Matheney <barry_matheney_msdn@outlook.com>
2020-06-17 22:08:15 -0400

    added conky files for ubuntu

---
+++ ubuntu/conky.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://linuxconfig.org/ubuntu-20-04-system-monitoring-with-conky-widgets#:~:text=Conky%20is%20a%20system%20monitoring,little%20widget%20on%20your%20screen.
+sudo apt install conky-all -y
+
+cp __homedirectorydotconfigs/.conkyrc  ~/
+cp __homedirectorydotconfigs/.xsessionrc  ~/ && chmod +x ~/.xsessionrc
+
+echo done
+
+
+
+
