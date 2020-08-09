d15042868de0aaedb9a5185c8f8aaadaa2d51071
Barry Matheney <barry_matheney_msdn@outlook.com>
2020-08-09 16:31:39 -0400

    updates following lab server build

--- serverubuntu2004/01_nogui.sh
+++ serverubuntu2004/01_nogui.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
 printf 'bmatheney ALL=(ALL:ALL) NOPASSWD:ALL\n' | sudo tee -a /etc/sudoers >/dev/null
-
-#sudo apt update -y
-#sudo apt upgrade -y
-sudo apt install -y  git htop curl wget  \
-figlet \
-dos2unix \
-ansible 
-# docker.io \
-
-
 git config --global user.name "Barry Matheney"
 git config --global user.email "barry_matheney_msdn@outlook.com"
+
+sudo apt update
+sudo apt install net-tools nmap tasksel - y
+tasksel --list-tasks
+sudo tasksel install xubuntu-desktop
 
 
 
--- ubuntu/01_gui.sh
+++ ubuntu/01_gui.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get -y  install  gitk  meld  xclip 
+## xrdp source:  https://linuxconfig.org/ubuntu-20-04-remote-desktop-access-from-windows-10
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get -y  install  \
+  gitk \
+  meld \
+  xclip \
+  gparted \
+  xfce4-goodies \
+  xrdp
+sudo systemctl enable --now xrdp
+sudo ufw allow from any to any port 3389 proto tcp
 
  
