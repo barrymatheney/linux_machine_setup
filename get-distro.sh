5f98500b51e0f72b85318a976db54ad9e1bbe9a2
Barry Matheney <barry_matheney_msdn@outlook.com>
2020-08-10 20:36:06 -0400

    20200810_203606

--- get-distro.sh
+++ get-distro.sh
@@ -1,12 +1,9 @@
-echo "this is not done"
+#!/bin/bash
+A=$(inxi -F | head )
+#echo $A
+distro=$(echo $A | cut -d":" -f7)
+echo Distro:  $(echo $distro  | sed 's|\(.*\) .*|\1|')
+kernel=$(echo $A | cut -d":" -f4)
+echo Kernel:  $(echo $kernel  | sed 's|\(.*\) .*|\1|')
 
-# A=$(inxi -F | head )
-# echo $A
-# B=$(echo $A | cut -d":" -f2)
-# echo $B
-
-
-
-# echo $OS
-# echo $VER
 
